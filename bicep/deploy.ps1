d41f169ee40e6886e8dfa1aaf2f21b13a692b578
Daniel Fang <qkfang@hotmail.com>
2026-03-21 08:09:15 +1100

    fix

--- bicep/deploy.ps1
+++ bicep/deploy.ps1
@@ -1,4 +1,4 @@
 
-az group create --name "rg-aigenius" --location "eastus2" 
+az group create --name "rg-aigenius-dev" --location "eastus2" 
 
-az deployment group create --resource-group "rg-aigenius" --template-file bicep/main.bicep --parameters "bicep/main.parameters.json"
+az deployment group create --resource-group "rg-aigenius-dev" --template-file bicep/main.bicep --parameters "bicep/main.parameters.json"
